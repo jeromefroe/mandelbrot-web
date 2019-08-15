25615e5f130a746acfff70825a0b18f02bb13a79
Jerome Froelich <jeromefroe@uber.com>
2019-08-15 09:25:08 -0400

    Remove Scope as it is unnecessary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![feature(async_await)]
 
-use std::marker::PhantomData;
-use std::pin::Pin;
 use std::time::Duration;
 
 use futures::future::join_all;
@@ -40,19 +38,19 @@ async fn handler(mut socket: TcpStream) {
         let mut pixels: [i32; 5] = [1, 2, 3, 4, 5];
 
         let shards: Vec<&mut i32> = pixels.iter_mut().collect();
+        let mut futures = Vec::with_capacity(shards.len());
 
-        scope(|s| {
-            for shard in shards.into_iter() {
-                s.spawn(async move {
-                    let millis = rand::thread_rng().gen_range(0, 100);
-                    let mut interval = Interval::new_interval(Duration::from_millis(millis));
-                    interval.next().await;
+        for shard in shards.into_iter() {
+            futures.push(async move {
+                let millis = rand::thread_rng().gen_range(0, 100);
+                let mut interval = Interval::new_interval(Duration::from_millis(millis));
+                interval.next().await;
 
-                    cpu_intesive_work(shard);
-                });
-            }
-        })
-        .await;
+                cpu_intesive_work(shard);
+            })
+        }
+
+        join_all(futures).await;
 
         // Write the data back
         if let Err(e) = socket.write_all(&buf[0..n]).await {
@@ -65,50 +63,4 @@ async fn handler(mut socket: TcpStream) {
 fn cpu_intesive_work(num: &mut i32) {
     *num *= 2;
     println!("num: {}", *num);
-}
-
-struct Scope<'env> {
-    futures: Vec<Pin<Box<dyn Future<Output = ()> + Send + 'static>>>,
-    _marker: PhantomData<&'env ()>,
-}
-
-impl<'env> Scope<'env> {
-    fn new() -> Scope<'env> {
-        Scope {
-            futures: Vec::new(),
-            _marker: PhantomData,
-        }
-    }
-
-    fn spawn<'scope, F>(&'scope mut self, f: F)
-    where
-        F: Future<Output = ()> + Send + 'env,
-        'env: 'scope,
-    {
-        // Use transmute to change the lifetime bound of the Future to 'static. The lifetime
-        // bound 'env: 'scope ensures that the lifetime 'env is longer than the lifetime 'scope.
-        // And our call to `join_all` below ensures that no future has a lifetime longer 'scope.
-        // Thus, even though we erase the 'env lifetime of the future we manually ensure that
-        // it's lifetime is less than 'env.
-        let func: Pin<Box<dyn Future<Output = ()> + Send + 'env>> = Box::pin(f);
-        let func: Pin<Box<dyn Future<Output = ()> + Send + 'static>> =
-            unsafe { std::mem::transmute(func) };
-        self.futures.push(func);
-    }
-
-    async fn join_all(self) {
-        // Ideally we would implement Drop for Scope and call join_all there so we can guarantee
-        // the function is called when Scope goes out of scope. As best as I can tell though that
-        // is not possible at the moment since we need to call await.
-        join_all(self.futures).await;
-    }
-}
-
-async fn scope<'env, F>(f: F)
-where
-    F: FnOnce(&mut Scope<'env>),
-{
-    let mut scope = Scope::new();
-    f(&mut scope);
-    scope.join_all().await;
 }
